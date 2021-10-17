eea0dea35306702920f2a86e84e632bc72f3b2a3
Hoang <htn1205@gmail.com>
2021-10-17 18:58:50 +0700

    1st update

---
+++ README.md
@@ -0,0 +1,36 @@
+# Friday Project
+
+This Friday project is Front End Development exercise utilize Met Museum API to design a website dedicated educate and appreciate the valuable art works at the museum.
+
+## File Structure
+
+```
+|- friday/
+        |- index.html
+        |- style.css
+        |- script.js/
+            |-images.png
+        |- detail-page/
+            |- index.html
+            |- style.css
+            |- script.js
+```
+
+## UI Mockup
+
+## Feature Overview
+
+- Landing page introduces the special feature of the website: Artwork of the month (carousel). "Explore more" button.
+- There is a search bar for user to search specific topic.
+- click "Explore more", the user will a list of different artworks and their basic information.
+- Each artwork cards will have a "more detail" button to head the user to their detail information page.
+
+## Endpoint Usage
+
+- /objects: for all archives
+- /search" with q= for keyword search
+
+## Installation
+
+- Clone this repo
+- Run the following
---
+++ script.js
@@ -0,0 +1,139 @@
+const API_KEY = "AIzaSyAY_uFHtButPFcseO1eA99z15qDrfasy9Y"
+const baseURL = "https://www.googleapis.com/books";
+const path = "/v1/volumes"
+const params = "?q="
+const authorParam = "&key="
+const searchBtn = document.getElementById("search-btn")
+const searchBox = document.getElementById("search-box")
+
+
+const handleSearch = (e) => {
+    e.preventDefault();
+    const searchContent = searchBox.value;
+    const fetchAsync = async () => {
+        const url = baseURL + path + params + searchContent + authorParam + API_KEY;
+        let response = await fetch(url);
+        let data = await response.json();
+        const books = data.items;
+        const booksInfo = books.map((e) => { return e.volumeInfo})
+        const resultHTML = document.getElementById("result-group")
+        const htmlOutput = booksInfo.map((singleBook) => {
+            return renderBooks(singleBook)
+        })
+        resultHTML.innerHTML = htmlOutput.join("");
+        return;
+    }
+
+    function renderBooks(singleBook) {
+        return `
+        <div class="col">
+            <div class="card myCard">
+                <img src="${singleBook.imageLinks.thumbnail}" class="card-img-top" atl="Book cover">
+                <div class="card-body">
+                <h5 class="card-title">${singleBook.title}</h5>
+                <p class="card-text">${singleBook.authors}</p>
+                <a href="detail/index.html">Read more</a>
+                </div>
+            </div>
+        </div>
+        `
+    }
+    fetchAsync();
+};
+
+searchBtn.addEventListener("click", handleSearch);
+
+const comedy = async() => {
+    const url = baseURL + path + params + "comedy" + authorParam + API_KEY;
+    let response = await fetch(url);
+    let data = await response.json();
+    let comedyBooks = data.items;
+    const comedyInfo = comedyBooks.map((e) => { return e.volumeInfo})
+    const comedySection = document.getElementById("comedy");
+    const htmlOutput = comedyInfo.map((e) => {
+        return renderComedy(e);
+    })
+    comedySection.innerHTML = htmlOutput.join("")
+    
+    function renderComedy(singleComedy) {
+        return `
+        <div class="row flex-row flex-nowrap">
+            <div class="col">
+                    <div class="card myCard">
+                        <img src="${singleComedy.imageLinks.thumbnail}" class="card-img-top" atl="Book cover">
+                        <div class="card-body">
+                        <h5 class="card-title">${singleComedy.title}</h5>
+                        <p class="card-text">${singleComedy.authors}</p>
+                        <a href="detail/index.html">Read more</a>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        `
+    };
+}
+comedy()
+
+const romance = async() => {
+        const url = baseURL + path + params + "romance" + authorParam + API_KEY;
+        let response = await fetch(url);
+        let data = await response.json();
+        const books = data.items;
+        const booksInfo = books.map((e) => { return e.volumeInfo})
+        const resultHTML = document.getElementById("romance")
+        const htmlOutput = booksInfo.map((singleBook) => {
+            return renderBooks(singleBook)
+        })
+        resultHTML.innerHTML = htmlOutput.join("");
+        return;
+    }
+    function renderBooks(singleBook) {
+        return `
+        <div class="row flex-row flex-nowrap">
+            <div class="col">
+                    <div class="card myCard">
+                        <img src="${singleBook.imageLinks.thumbnail}" class="card-img-top" atl="Book cover">
+                        <div class="card-body">
+                        <h5 class="card-title">${singleBook.title}</h5>
+                        <p class="card-text">${singleBook.authors}</p>
+                        <a href="detail/index.html">Read more</a>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        `
+};
+
+romance()
+
+const business = async() => {
+    const url = baseURL + path + params + "business" + authorParam + API_KEY;
+    let response = await fetch(url);
+    let data = await response.json();
+    let businessBooks = data.items;
+    const businessInfo = businessBooks.map((e) => { return e.volumeInfo})
+    const businessSection = document.getElementById("business");
+    const htmlOutput = businessInfo.map((e) => {
+        console.log(e)
+        return renderBusiness(e);
+    })
+    businessSection.innerHTML = htmlOutput.join("")
+    
+    function renderBusiness(singleBusiness) {
+        return `
+        <div class="row flex-row flex-nowrap">
+            <div class="col">
+                    <div class="card myCard">
+                        <img src="${singleBusiness.imageLinks.thumbnail}" class="card-img-top" atl="Book cover">
+                        <div class="card-body">
+                        <h5 class="card-title">${singleBusiness.title}</h5>
+                        <p class="card-text">${singleBusiness.authors}</p>
+                        <a href="detail/index.html">Read more</a>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        `
+    };
+}
+business()
